093f171164a00ace357fae8c168cb7bef047c849
Miguel Ángel de la Rosa <migueeldrg@gmail.com>
2023-07-06 10:52:21 -0600

    feat: modify

--- utils/test.ts
+++ utils/test.ts
@@ -1,3 +1,4 @@
-import { map, multiply } from "ramda";
+import { map, multiply } from 'ramda'
 
-export const doubleArrayValues = (arr) => map(multiply(2), arr);
+export const doubleArrayValues = (arr) => map(multiply(2), arr)
+console.log('hola')
